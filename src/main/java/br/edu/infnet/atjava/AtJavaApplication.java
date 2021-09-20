8cb46bdad2ec560ba354c99e39e3169cc723bcf0
Aline Souza <ds.alinesouza@gmail.com>
2021-09-20 20:52:51 -0300

    acrescenta anotacao

--- src/main/java/br/edu/infnet/atjava/AtJavaApplication.java
+++ src/main/java/br/edu/infnet/atjava/AtJavaApplication.java
@@ -5,10 +5,12 @@ import br.edu.infnet.domain.service.UsuarioService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableFeignClients
 @ComponentScan(basePackageClasses = {AcessoController.class, UsuarioService.class})
 @EntityScan(basePackages = {"br.edu.infnet.domain.model"})
 @EnableJpaRepositories(basePackages = {"br.edu.infnet.domain.repository"})
