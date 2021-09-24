5ca040e29944af4167a9d64b4b7ebe0641bf61fc
Aline Souza <ds.alinesouza@gmail.com>
2021-09-23 21:03:42 -0300

    atualiza repository

--- src/main/java/br/edu/infnet/domain/repository/AcaoRepository.java
+++ src/main/java/br/edu/infnet/domain/repository/AcaoRepository.java
@@ -1,9 +1,15 @@
 package br.edu.infnet.domain.repository;
 
 import br.edu.infnet.domain.model.Acao;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AcaoRepository extends CrudRepository<Acao, Integer> {
+
+    @Query("from Acao a where a.usuario.id = :userId")
+    public List<Acao> obterLista(Integer userId);
 }
