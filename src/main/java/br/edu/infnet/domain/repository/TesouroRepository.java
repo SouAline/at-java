1baa533360d7c411ff226040a5bd34da50d122b3
Aline Souza <ds.alinesouza@gmail.com>
2021-09-23 21:07:57 -0300

    atualiza repository adiciona query

--- src/main/java/br/edu/infnet/domain/repository/TesouroRepository.java
+++ src/main/java/br/edu/infnet/domain/repository/TesouroRepository.java
@@ -1,10 +1,15 @@
 package br.edu.infnet.domain.repository;
 
 import br.edu.infnet.domain.model.TesouroDireto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TesouroRepository extends CrudRepository<TesouroDireto, Integer> {
+    @Query("from TesouroDireto td where td.usuario.id = :userId")
+    public List<TesouroDireto> obterLista(Integer userId);
 
 }
