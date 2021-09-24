87d3a535d64fdd6afbda295eca28418c972d30a0
Aline Souza <ds.alinesouza@gmail.com>
2021-09-23 21:06:22 -0300

    atualiza repository

--- src/main/java/br/edu/infnet/domain/repository/FundoImobiliarioRepository.java
+++ src/main/java/br/edu/infnet/domain/repository/FundoImobiliarioRepository.java
@@ -2,9 +2,14 @@ package br.edu.infnet.domain.repository;
 
 
 import br.edu.infnet.domain.model.FundoImobiliario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FundoImobiliarioRepository extends CrudRepository<FundoImobiliario, Integer> {
+    @Query("from FundoImobiliario f where f.usuario.id = :userId")
+    public List<FundoImobiliario> obterLista(Integer userId);
 }
